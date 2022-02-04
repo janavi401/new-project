d0e6bc8cab75db9a8ed892b5aaa8eca3e72c8b54
Janavi Reddy <Janavi Reddy@DESKTOP-73OQVUV>
2022-02-04 10:53:13 +0530

    example1 commit

---
+++ new project/src/com/javaBasics/example1.java
@@ -0,0 +1,13 @@
+package com.javaBasics;
+
+public class example1 {
+
+	public static void main(String[] args)
+	{
+		// write a java program to print hello java
+		
+		System.out.println("hello java");
+
+	}
+
+}
